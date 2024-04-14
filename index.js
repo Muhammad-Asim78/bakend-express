f09d9e328a3efb20b89d8cd6b345cd029ccb0087
Muhammad-Asim78 <asimq1466@gmail.com>
2024-04-14 13:31:14 +0500

    add data

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/twitter',(req,res)=>{
+    res.send("<h1> This is Twitter Page </h2>")
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
